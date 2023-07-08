0a391f26044274a292b92a341abe94a7f158e0ad
Alexander Bösecke <boesi@users.noreply.github.com>
2023-07-08 20:22:29 +0200

    PxWindow now has a close and minimize button

--- app/main.mjs
+++ app/main.mjs
@@ -4,6 +4,6 @@ import Settings from './modules/settings.mjs';
 import config from './config.js';
 
 document.title = `${config.name} ${config.version}`
-let pxWindow = new PxWindow(`${config.name} ${config.version} Settings`);
+let pxWindow = new PxWindow(`${config.name} ${config.version} Settings`, {closeable: false, minimizeable: true});
 let settings = new Settings();
 pxWindow.addContent(settings.content);
