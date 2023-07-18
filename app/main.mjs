fa192c2f8c7c9ef58a9669dd5dd7e8bd6aebf282
boesi <you@example.com>
2023-07-18 13:12:41 +0200

    introduce board-data to make settings and storage-settings truly independent

--- app/main.mjs
+++ app/main.mjs
@@ -2,17 +2,19 @@
 import PxWindow from './modules/components/px-window.mjs';
 import PxTabs from './modules/components/px-tabs.mjs';
 import Settings from './modules/settings.mjs';
-import StorageSettings from './modules/storage-settings.mjs';
+import StorageSettings from './modules/storage/settings.mjs';
 import config from './config.js';
+import BoardData from './modules/board-data.mjs';
 
 function main() {
 	document.title = `${config.name} ${config.version}`
 	let pxWindow = new PxWindow(`${config.name} ${config.version} Settings`, {closeable: false, minimizeable: true});
 	let pxTabs = new PxTabs();
 	pxWindow.addContent(pxTabs.content);
-	let settings = new Settings();
+	let boardData = new BoardData();
+	let settings = new Settings(boardData);
 	pxTabs.addTab('Walkers', settings.content);
-	let storage = new StorageSettings();
+	let storage = new StorageSettings(boardData);
 	pxTabs.addTab('Storage', storage.content);
 }
 
