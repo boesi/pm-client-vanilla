7185965e8103414a11a0c620896357fd9adf27e1
boesi <you@example.com>
2023-07-19 17:53:24 +0200

    create a separate class for selecting a board - made use of createContextualFragment

--- app/modules/settings.mjs
+++ app/modules/settings.mjs
@@ -1,62 +1,26 @@
 import Walker from './walker.mjs';
 import WalkerController from './walker-controller.mjs';
-import BoardSVG from '/modules/board/svg.mjs';
-import BoardHTML from '/modules/board/html.mjs';
-import BoardCanvas from '/modules/board/canvas.mjs';
+import BoardSelector from '/modules/board/selector.mjs';
 
 class Settings {
-	boardData = null;
+	#boardData = null;
 	wndSetting = document.createElement('div');
 	wndWalkerList = document.createElement('div');
 
 	constructor(boardData) {
-		this.boardData = boardData;
+		this.#boardData = boardData;
 		this.wndSetting.classList.add('settings');
-		this.wndSetting.insertAdjacentHTML('afterbegin', `
-			<label id="board-type">
-				Board:
-				<select>
-					<option value="---">---</option>
-					<option value="canvas">Canvas</option>
-					<option value="html">HTML</option>
-					<option value="svg">SVG</option>
-				</select>
-			</label>
-		`);
+		let wndSelector = new BoardSelector(boardData, this.createWalker.bind(this));
+		this.wndSetting.append(wndSelector.content);
 		this.wndSetting.append(this.wndWalkerList);
-		let selectorBoard = this.wndSetting.querySelector('#board-type select');
-		this.bindEvents();
 
-		selectorBoard.addEventListener('input', this.selectBoard);
-		document.addEventListener('new-walkers', this.receiveWalkers);
-	}
-
-	bindEvents() {
-		this.createWalker = this.createWalker.bind(this);
-		this.selectBoard = this.selectBoard.bind(this);
-		this.receiveWalkers = this.receiveWalkers.bind(this);
+		document.addEventListener('new-walkers', this.receiveWalkers.bind(this));
 	}
 
 	get content() {
 		return this.wndSetting;
 	}
 
-
-	selectBoard(event) {
-		switch (event.srcElement.value) {
-			case 'canvas':
-				this.boardData.setBoard(new BoardCanvas(this.createWalker));
-				break;
-			case 'html':
-				this.boardData.setBoard(new BoardHTML(this.createWalker));
-				break;
-			case 'svg':
-				this.boardData.setBoard(new BoardSVG(this.createWalker));
-				break;
-			default:
-				break;
-		}
-	}
 
 	receiveWalkers(event) {
 		for (let walkerData of event.detail.walkers) {
@@ -65,12 +29,12 @@ class Settings {
 	}
 
 	createWalker(event) {
-		this.createWalkerController(new Walker({x: event.pageX, y: event.pageY, size: this.boardData.size}));
+		this.createWalkerController(new Walker({x: event.pageX, y: event.pageY, size: this.#boardData.size}));
 	}
 
 	createWalkerController(walker) {
 		let walkerController = new WalkerController(walker);
-		this.boardData.addWalkerController(walkerController);
+		this.#boardData.addWalkerController(walkerController);
 		this.wndWalkerList.append(walkerController.controller);
 		walkerController.start();
 	}
