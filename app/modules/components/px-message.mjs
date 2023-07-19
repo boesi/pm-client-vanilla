65ba98b52bed92c970b978f4e0e7be51cdcca287
boesi <you@example.com>
2023-07-19 21:35:07 +0200

    px-message can hide automatically

--- app/modules/components/px-message.mjs
+++ app/modules/components/px-message.mjs
@@ -3,22 +3,29 @@ import CSSHelpers from '/modules/utils/css-helpers.mjs';
 class PxMessage {
 	wndContent = document.createElement('div');
 	messageTypes = ['info', 'error', 'warning'];
+	pxMessageTypes = this.messageTypes.map(aType => 'px-' + aType);
 	
 	constructor() {
 		this.wndContent.classList.add('px-message');
 		CSSHelpers.addCSS('modules/components/px-message.css');
+		this.clear = this.clear.bind(this);
 	}
 
 	get content() {
 		return this.wndContent;
 	}
 
-	setMessage(type, message) {
+	setMessage(type, message, options = {}) {
+		this.setPosition(options);
 		this.wndContent.textContent = message;
 		this.wndContent.classList.remove(
-			...this.messageTypes.filter(aType => aType !== type).map(aType => 'px-' + aType)
+			...this.pxMessageTypes.filter(aType => aType !== 'px-' + type)
 		);
 		this.wndContent.classList.add('px-' + type, 'px-show');
+		if (options.autohide) {
+			this.wndContent.classList.add('px-hide');
+			this.wndContent.addEventListener('animationend', this.clear);
+		}
 	}
 
 	setPosition({x, y} = {}) {
@@ -32,18 +39,21 @@ class PxMessage {
 	}
 
 	setInfo(message, options = {}) {
-		this.setMessage('info', message);
-		this.setPosition(options);
+		this.setMessage('info', message, options);
 	}
 
 	setError(message, options = {}) {
-		this.setMessage('error', message);
-		this.setPosition(options);
+		this.setMessage('error', message, options);
 	}
 
 	clear() {
+		console.log('===> PxMessage.clear');
 		this.wndContent.textContent = '';
-		this.wndContent.classList.remove('px-show');
+		this.wndContent.classList.remove('px-show', 'px-hide', 'px-absolute', ...this.pxMessageTypes);
+		this.wndContent.removeAttribute('style');
+		//this.wndContent.style['left'] = null;
+		//this.wndContent.style['top'] = null;
+		this.wndContent.removeEventListener('animationend', this.clear);
 	}
 }
 
