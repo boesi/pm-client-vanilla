bde3515b690d7c28ac71c07b7acc2e61509bc397
Alexander Bösecke <boesi@users.noreply.github.com>
2023-07-20 11:24:17 +0200

    load css file only if it is not already loaded

--- app/modules/utils/css-helpers.mjs
+++ app/modules/utils/css-helpers.mjs
@@ -1,12 +1,23 @@
 class CSSHelpers {
 	static #head = document.getElementsByTagName('HEAD')[0];
 
+	static getCSS(file) {
+		let links = document.getElementsByTagName('link');
+		for (let link of links) {
+			if (link.href.substr(-file.length) == file) return link;
+		}
+		return null;
+	}
+
 	static addCSS(file) {
-		var link = document.createElement('link');
-		link.rel = 'stylesheet';
-		link.type = 'text/css';
-		link.href = file;
-		CSSHelpers.#head.appendChild(link);
+		let link = this.getCSS(file);
+		if (! link) {
+			link = document.createElement('link');
+			link.rel = 'stylesheet';
+			link.type = 'text/css';
+			link.href = file;
+			CSSHelpers.#head.appendChild(link);
+		}
 		return link;
 	}
 
