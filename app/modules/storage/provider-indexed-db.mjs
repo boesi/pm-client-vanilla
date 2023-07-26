7cc19728257cc9aa8dee8b5ad4782e56e71401f2
Alexander Bösecke <boesi@users.noreply.github.com>
2023-07-26 18:24:34 +0200

    ProviderIndexedDb now wraps the events in promises

--- app/modules/storage/provider-indexed-db.mjs
+++ app/modules/storage/provider-indexed-db.mjs
@@ -4,54 +4,55 @@ class ProviderIndexedDb {
 	#storeName = 'PixelMoverData';
 	#storeVersion = 1;
 
-	onload = null;
-
-	#error = event => {
-		console.error('===> ProviderIndexedDb.error', {event, 'this': this});
-	};
-
-	#successOpen = event => {
-		this.#db = event.target.result;
-		// we use a central error handler for all requests and transactions
-		this.#db.onerror = this.#error;
-	};
-	
-	#upgrade = event => {
-		event.target.result.
-			createObjectStore(this.#storeName, {keyPath: 'name'}).
+	#handleUpgrade = event => {
+		console.info('===> ProviderIndexedDb.handleUpgrade', {event});
+		const db = event.target.result;
+		if (event.oldVersion > 0 && event.oldVersion < this.#storeVersion) {
+			db.deleteObjectStore(this.#storeName);
+		}
+		db.createObjectStore(this.#storeName, {keyPath: 'name'}).
 			createIndex('name', 'name', {unique: true});
 	};
 
-	constructor() {
+	#open() {
 		const request = window.indexedDB.open(this.#storeName, this.#storeVersion);
-		request.onerror = this.#error;
-		request.onsuccess = this.#successOpen;
-		request.onupgradeneeded = this.#upgrade;
+		request.onupgradeneeded = this.#handleUpgrade;
+		return this.#toPromise(request);
 	}
 
-	#transactionStore(mode) {
-		const store = this.#db?.transaction([this.#storeName], mode).objectStore(this.#storeName);
+	async #transactionStore(mode) {
+		if (! this.#db) {
+			this.#db = await this.#open();
+		}
+		const store = this.#db.transaction([this.#storeName], mode).objectStore(this.#storeName);
 		if (!store) throw new Error('Can not access database, transaction or store');
 		return store;
 	}
 
-	save(data) {
-		this.#transactionStore('readwrite').
-			put(data);
+	#toPromise(request) {
+		return new Promise((resolve, reject) => {
+			request.onsuccess = event => resolve(event.target.result);
+			// request.onerror = event => reject(event.target.erorr);
+			request.onerror = event => {
+				console.log('===> ProviderIndexedDb.toPromise.onerror', {event});
+				reject(event.target.error);
+			}
+		});
 	}
 
-	load(name) {
-		this.#transactionStore('readonly').
-			get(name).
-			onsuccess = event => {
-				this.onload?.(event.target.result);
-			};
-		return this;
+	async save(data) {
+		let store = await this.#transactionStore('readwrite');
+		return this.#toPromise(store.put(data));
 	}
 
-	remove(name) {
-		this.#transactionStore('readwrite').
-			delete(name);
+	async load(name) {
+		let store = await this.#transactionStore('readonly');
+		return this.#toPromise(store.get(name));
+	}
+
+	async remove(name) {
+		let store = await this.#transactionStore('readwrite');
+		return this.#toPromise(store.delete(name));
 	}
 }
 
