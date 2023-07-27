248658827e81ab12a7e64a93f4910b3dfa150b77
boesi <you@example.com>
2023-07-27 22:30:37 +0200

    PxMessage can show details for error

--- app/modules/components/px-message.mjs
+++ app/modules/components/px-message.mjs
@@ -1,57 +1,80 @@
 import CSSHelpers from '/modules/utils/css-helpers.mjs';
 
 class PxMessage {
-	wndContent = document.createElement('div');
-	messageTypes = ['info', 'error', 'warning'];
-	pxMessageTypes = this.messageTypes.map(aType => 'px-' + aType);
+	#wndContent = document.createRange().createContextualFragment(`
+		<div class="px-message">
+			<div>
+				<span class="message"></span>
+				<span class="detail-button"></span>
+			</div>
+			<div class="detail"></div>
+		</div>
+	`);
+	#wndMessage = this.#wndContent.querySelector('.message');
+	#wndDetail = this.#wndContent.querySelector('.detail');
+	#wndDetailButton = this.#wndContent.querySelector('.detail-button');
+	#messageTypes = ['info', 'error', 'warning'];
+	#pxMessageTypes = this.#messageTypes.map(aType => 'px-' + aType);
 	
 	constructor() {
-		this.wndContent.classList.add('px-message');
 		CSSHelpers.addCSS('modules/components/px-message.css');
-		this.clear = this.clear.bind(this);
+		this.#wndContent = this.#wndContent.querySelector('.px-message');
+		this.#wndDetailButton.addEventListener('click', this.#showDetail);
 	}
 
 	get content() {
-		return this.wndContent;
+		return this.#wndContent;
 	}
 
-	setMessage(type, message, options = {}) {
-		this.setPosition(options);
-		this.wndContent.textContent = message;
-		this.wndContent.classList.remove(
-			...this.pxMessageTypes.filter(aType => aType !== 'px-' + type)
+	#showDetail = event => {
+		this.#wndDetail.classList.toggle('px-show');
+		this.#wndDetailButton.classList.toggle('selected');
+	};
+
+	#setMessage(type, message, options = {}) {
+		this.#setPosition(options);
+		this.#wndMessage.textContent = message;
+		this.#wndContent.classList.remove(
+			...this.#pxMessageTypes.filter(aType => aType !== 'px-' + type)
 		);
-		this.wndContent.classList.add('px-' + type, 'px-show');
+		this.#wndContent.classList.add('px-' + type, 'px-show');
 		if (options.autohide) {
-			this.wndContent.classList.add('px-hide');
-			this.wndContent.addEventListener('animationend', this.clear);
+			this.#wndContent.classList.add('px-hide');
+			this.#wndContent.addEventListener('animationend', this.clear);
+		}
+		if (options.error) {
+			this.#wndDetailButton.classList.add('px-show');
+			this.#wndDetail.textContent = options.error.message;
+		} else {
+			this.#wndDetailButton.classList.remove('px-show');
+			this.#wndDetail.classList.remove('px-show');
 		}
 	}
 
-	setPosition({x, y} = {}) {
+	#setPosition({x, y} = {}) {
 		if (x == null && y == null) {
-			this.wndContent.classList.remove('px-absolute');
+			this.#wndContent.classList.remove('px-absolute');
 		} else {
-			this.wndContent.classList.add('px-absolute');
+			this.#wndContent.classList.add('px-absolute');
 		}
-		this.wndContent.style['left'] = x == null ? null : `${x}px`;
-		this.wndContent.style['top'] = y == null ? null : `${y}px`;
+		this.#wndContent.style['left'] = x == null ? null : `${x}px`;
+		this.#wndContent.style['top'] = y == null ? null : `${y}px`;
 	}
 
 	setInfo(message, options = {}) {
-		this.setMessage('info', message, options);
+		this.#setMessage('info', message, options);
 	}
 
 	setError(message, options = {}) {
-		this.setMessage('error', message, options);
+		this.#setMessage('error', message, options);
 	}
 
-	clear() {
-		this.wndContent.textContent = '';
-		this.wndContent.classList.remove('px-show', 'px-hide', 'px-absolute', ...this.pxMessageTypes);
-		this.wndContent.removeAttribute('style');
-		this.wndContent.removeEventListener('animationend', this.clear);
-	}
+	clear = () => {
+		this.#wndMessage.textContent = '';
+		this.#wndContent.classList.remove('px-show', 'px-hide', 'px-absolute', ...this.#pxMessageTypes);
+		this.#wndContent.removeAttribute('style');
+		this.#wndContent.removeEventListener('animationend', this.clear);
+	};
 }
 
 export default PxMessage;
