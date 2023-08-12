0a17973160c6fffa7478d005dba877747c36947f
Alexander Bösecke <boesi@users.noreply.github.com>
2023-08-12 20:38:40 +0200

    ProviderFile with new method support

--- app/modules/storage/provider-file.mjs
+++ app/modules/storage/provider-file.mjs
@@ -1,5 +1,14 @@
 class ProviderFile {
 
+	/**
+	 * How can I use this method? Knowing if this provider is usable on this
+	 * system in advance, is clearly an advantage. But where should I call this
+	 * method?
+	 */
+	supported() {
+		return window.showOpenFilePicker === 'function';
+	}
+
 	save(data) {
 	}
 
