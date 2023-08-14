9e95398c1b6051b8b9231377ccaea32dbe72e798
Alexander Bösecke <boesi@users.noreply.github.com>
2023-08-14 06:29:58 +0200

    ProviderOpfs: save data

---
+++ app/modules/storage/provider-opfs.mjs
@@ -0,0 +1,32 @@
+class ProviderOpfs {
+
+	/**
+	 * How can I use this method? Knowing if this provider is usable on this
+	 * system in advance, is clearly an advantage. But where should I call this
+	 * method?
+	 */
+	supported() {
+		return navigator.storage.getDirectory === 'function';
+	}
+
+	async save(data) {
+		let stream = null;
+		try {
+			const handle = await navigator.storage.getDirectory();
+			const file = await handle.getFileHandle(data.name, {create: true});
+			stream = await file.createWritable();
+			stream.write(JSON.stringify(data));
+		} finally {
+			if (stream !== null) stream.close();
+		}
+	}
+
+	async load(name) {
+		const handle = await navigator.storage.getDirectory();
+	}
+
+	remove(name) {
+	}
+}
+
+export default ProviderOpfs;
