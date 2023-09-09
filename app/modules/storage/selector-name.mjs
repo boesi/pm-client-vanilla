640a79bfac8e46b527af47cf660f4f5bda3775f9
Alexander Bösecke <boesi@users.noreply.github.com>
2023-09-09 12:41:34 +0200

    SelectorName shows a datalist

--- app/modules/storage/selector-name.mjs
+++ app/modules/storage/selector-name.mjs
@@ -2,13 +2,18 @@ class StorageSelectorName {
 	#content = document.createRange().createContextualFragment(`
 		<label for="input-storage-selector-name">
 			Name: 
-			<input id="input-storage-selector-name"/>
+			<input list="storage-selector-names" id="input-storage-selector-name"/>
 		</label>
+		<datalist id="storage-selector-names">
+			<option value="test"/>
+		</datalist>
 	`);
 	#input = null;
+	#lstNames = null;
 
 	constructor() {
-		this.#input = this.#content.querySelector('#input-storage-selector-name');
+		this.#input = this.#content.querySelector("#input-storage-selector-name");
+		this.#lstNames = this.#content.querySelector("#storage-selector-names");
 	}
 
 	get content() {
