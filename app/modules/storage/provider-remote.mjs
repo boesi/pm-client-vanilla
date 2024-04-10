a8aa1471fb9cd4ae16cde04353b1a8269cbd6c5b
Alexander Bösecke <boesi@users.noreply.github.com>
2024-04-10 21:33:36 +0200

    correct url for server

--- app/modules/storage/provider-remote.mjs
+++ app/modules/storage/provider-remote.mjs
@@ -1,9 +1,14 @@
 class ProviderRemote {
 
+	#url = '/pm-server-node-http';
+	#urlItem = this.#url + '/item';
+
 	constructor() {
 	}
 
 	async save(data) {
+		const response = await window.fetch(this.#urlItem, {method: 'PUT', body: JSON.stringify(data)});
+		console.log('===> storage.ProviderRemote.save', {response});
 	}
 
 	async getItems() {
