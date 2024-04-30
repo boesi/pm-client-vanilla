d8f921c4caddb2d2d02fba7d487e5a3cabb3ed0f
Alexander Bösecke <boesi@users.noreply.github.com>
2024-04-30 22:01:56 +0200

    give the status code with the error message

--- app/modules/storage/provider-remote.mjs
+++ app/modules/storage/provider-remote.mjs
@@ -8,20 +8,20 @@ class ProviderRemote {
 
 	async save(data) {
 		const response = await window.fetch(`${this.#urlItem}?name=${data.name}`, {method: 'PUT', body: JSON.stringify(data)});
-		if (! response.ok) throw new Error(response.statusText);
+		if (! response.ok) throw new Error(`${response.status}: ${response.statusText}`);
 		return response.ok;
 	}
 
 	async getItems() {
 		const response = await window.fetch(`${this.#url}`, {method: 'GET'});
-		if (! response.ok) throw new Error(response.statusText);
+		if (! response.ok) throw new Error(`${response.status}: ${response.statusText}`);
 		const json = await response.json();
 		return json.names;
 	}
 
 	async load(name) {
 		const response = await window.fetch(`${this.#urlItem}?name=${name}`, {method: 'GET'});
-		if (! response.ok) throw new Error(response.statusText);
+		if (! response.ok) throw new Error(`${response.status}: ${response.statusText}`);
 		return await response.json();
 	}
 
